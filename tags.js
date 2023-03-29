126ea80426e2953b0c683c04c29a5b747cc976c4
weizman <weizmangal@gmail.com>
2023-03-29 21:36:54 +0300

    forgot tags

---
+++ tags.js
@@ -0,0 +1,33 @@
+(function(){
+    const parent = document.currentScript.parentElement;
+
+    top.TAGS = {
+        ALL: [
+            'CVEs', 'Vulnerabilities', 'Security', 'JavaScript',
+            'Research', 'Anti-Debug', 'Videos', 'Published', 'Podcasts',
+            'Posts', 'News', 'Supply-Chain-Security',
+            'Discovery', 'Browser', 'Top', 'MetaMask', 'LavaMoat',
+        ],
+        SELECTED: new URLSearchParams(window.location.search).get('tags').split(','),
+    };
+
+    top.TAGS.ALL.forEach(tag => {
+        const selected = top.SELECTED.includes(tag);
+        const a = document.createElement('a');
+        a.style.fontWeight = selected ? 'bold' : '';
+        a.textContent += tag;
+        a.onclick = () => {
+            if (selected) {
+                top.SELECTED.splice(top.SELECTED.indexOf(tag), 1);
+            } else {
+                top.SELECTED.push(tag);
+            }
+            const searchParams = new URLSearchParams(window.location.search);
+            searchParams.set('tags', top.SELECTED.join(','));
+            window.location.search = searchParams.toString();
+        };
+        parent.appendChild(a);
+    });
+
+    document.currentScript.remove();
+}())
