e2358b0590faccaeb3bb84623b66b4a9bf66f6c4
weizman <weizmangal@gmail.com>
2024-01-14 10:02:48 +0200

    Update alert.js

--- public/alert.js
+++ public/alert.js
@@ -1,4 +1,9 @@
-alert(location.href.split('msg=')[1]);
+if (location.href.includes('silent') {
+  console.log('fetched script:', location.href.split('msg=')[1]);
+}
+else {
+  alert(location.href.split('msg=')[1]);
+}
 var xyz123_1 = 1;
 xyz123_2 = 1;
 const xyz123_3 = 1;
