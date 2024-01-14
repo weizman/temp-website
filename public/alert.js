b71d2997a5bad93916ed25c29c038f7f9ffe65f3
weizman <weizmangal@gmail.com>
2024-01-14 10:13:33 +0200

    Update alert.js

--- public/alert.js
+++ public/alert.js
@@ -4,7 +4,7 @@ if (location.href.includes('silent')) {
 else {
   alert(location.href.split('msg=')[1]);
 }
-var xyz123_1 = 1;
-xyz123_2 = 1;
-const xyz123_3 = 1;
-let xyz123_4 = 1;
+//var xyz123_1 = 1;
+//xyz123_2 = 1;
+//const xyz123_3 = 1;
+//let xyz123_4 = 1;
